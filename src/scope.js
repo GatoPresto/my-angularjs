bf7115c58409c886e4799d4b0af65033f3697a7b
Sergey Naumenko <usrgame@gmail.com>
2015-06-24 12:00:52 +0300

    Value-based dirty-cheking and NaNs.

--- src/scope.js
+++ src/scope.js
@@ -8,10 +8,11 @@ function Scope() {
 	this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
 	var watcher = {
 		watchFn: watchFn,
 		listenerFn: listenerFn || function() {},
+		valueEq: !!valueEq,
 		last: initWatchVal
 	};
 	this.$$watchers.push(watcher);
@@ -26,9 +27,9 @@ Scope.prototype.$$digestOnce = function() {
 		newValue = watcher.watchFn(self);
 		oldValue = watcher.last;
 
-		if ( newValue !== oldValue ) {
+		if ( !self.$$areEqual(newValue, oldValue, watcher.valueEq) ) {
 			self.$$lastDirtyWatch = watcher;
-			watcher.last = newValue;
+			watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
 			watcher.listenerFn(newValue,
 				(oldValue === initWatchVal ? newValue : oldValue),
 				self);
@@ -53,4 +54,14 @@ Scope.prototype.$digest = function() {
 			throw "10 digest iterations reached";
 		}
 	} while (dirty);
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+	if ( valueEq ) {
+		return _.isEqual(newValue, oldValue);
+	} else {
+		return newValue === oldValue ||
+			(typeof newValue === 'number' && typeof oldValue === 'number' &&
+				isNaN(newValue) && isNaN(oldValue));
+	}
 };
