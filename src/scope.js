e2c5dd52b15a33f5bb7bd0cec1aa7cf11203f2f0
Sergey Naumenko <usrgame@gmail.com>
2015-06-24 16:37:55 +0300

    $applyAsync

--- src/scope.js
+++ src/scope.js
@@ -7,6 +7,8 @@ function Scope() {
 	this.$$watchers = [];
 	this.$$lastDirtyWatch = null;
 	this.$$asyncQueue = [];
+	this.$$applyAsyncQueue = [];
+	this.$$applyAsyncId = null;
 	this.$$phase = null;
 }
 
@@ -48,8 +50,13 @@ Scope.prototype.$digest = function() {
 	var ttl = 10;
 	var dirty;
 	this.$$lastDirtyWatch = null;
-
 	this.$beginPhase("$digest");
+
+	if ( this.$$applyAsyncId ) {
+		clearTimeout(this.$$applyAsyncId);
+		this.$$flushApplyAsync();
+	}
+
 	do {
 		while (this.$$asyncQueue.length) {
 			var asyncTask = this.$$asyncQueue.shift();
@@ -113,4 +120,23 @@ Scope.prototype.$beginPhase = function(phase) {
 
 Scope.prototype.$clearPhase = function() {
 	this.$$phase = null;
+};
+
+Scope.prototype.$applyAsync = function(expr) {
+	var self = this;
+	self.$$applyAsyncQueue.push(function() {
+		self.$eval(expr);
+	});
+	if ( self.$$applyAsyncId === null ) {
+		self.$$applyAsyncId = setTimeout(function() {
+			self.$apply(_.bind(self.$$flushApplyAsync, self));
+		}, 0);
+	}
+};
+
+Scope.prototype.$$flushApplyAsync = function() {
+	while (this.$$applyAsyncQueue.length) {
+		this.$$applyAsyncQueue.shift()();
+	}
+	this.$$applyAsyncId = null;
 };
