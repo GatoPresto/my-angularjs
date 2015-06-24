5c42b76d23c7e2199d228ad90f702ca9da9c1bfd
Sergey Naumenko <usrgame@gmail.com>
2015-06-24 17:13:13 +0300

    Running code after a digest - $$postDigest

--- src/scope.js
+++ src/scope.js
@@ -9,6 +9,7 @@ function Scope() {
 	this.$$asyncQueue = [];
 	this.$$applyAsyncQueue = [];
 	this.$$applyAsyncId = null;
+	this.$$postDigestQueue = [];
 	this.$$phase = null;
 }
 
@@ -71,6 +72,10 @@ Scope.prototype.$digest = function() {
 		}
 	} while (dirty || this.$$asyncQueue.length);
 	this.$clearPhase();
+
+	while (this.$$postDigestQueue.length) {
+		this.$$postDigestQueue.shift()();
+	}
 };
 
 Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
@@ -139,4 +144,8 @@ Scope.prototype.$$flushApplyAsync = function() {
 		this.$$applyAsyncQueue.shift()();
 	}
 	this.$$applyAsyncId = null;
+};
+
+Scope.prototype.$$postDigest = function(fn) {
+	this.$$postDigestQueue.push(fn);
 };
